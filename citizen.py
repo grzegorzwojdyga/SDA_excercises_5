4ce497718a14472c294d5b675843079b068a5f61
grzegorzwojdyga <36237589+grzegorzwojdyga@users.noreply.github.com>
2022-03-17 17:12:43 +0100

    Add files via upload

---
+++ citizen.py
@@ -0,0 +1,11 @@
+"""
+    Napisz klasę Citizen, która implementuje metodę __init__, przyjmującą dwa argumenty: first name i last name.
+    Stwórz obiekt tej klasy, a następnie wyświetl imię i nazwisko obywatela.
+
+    Dodaj metodę klasową set_nationality do klasy Citizen (a wraz z nią pole klasowe nationality
+    o wybranej wartości domyślnej).
+    Wywołaj tę metodę. Stwórz drugi obiekt klasy i zobacz, jaką wartość będzie miało pole nationality w obu obiektach.
+
+    Dodatkowo możesz spróbować dodać atrybut klasowy total_number_of_citizens będący liczbą.
+    Zwiększaj ją za każdym razem, kiedy tworzony będzie nowy obywatel.
+"""
---
+++ rectangle.py
@@ -0,0 +1,4 @@
+"""
+    Stwórz klasę o nazwie Rectangle.
+    Klasa powinna mieć dwa pola dla boków oraz metodę area (property) do liczenia pola prostokąta.
+"""
